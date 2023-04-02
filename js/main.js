b184d5e37c5501ada145ae5a4fed02afd944d639
Nastya Galkina <55530369+nastyagalkina@users.noreply.github.com>
2023-04-02 17:57:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Header background color
 (function () {
   const header = document.querySelector(".header");
   window.onscroll = () => {
@@ -15,13 +14,21 @@
   const burgerItem = document.querySelector(".burger");
   const menu = document.querySelector(".header__nav");
   const menuCloseItem = document.querySelector(".header__nav-close");
+  const menuLinks = document.querySelectorAll(".header__link");
   burgerItem.addEventListener("click", () => {
     menu.classList.add("header__nav_active");
   });
-
   menuCloseItem.addEventListener("click", () => {
     menu.classList.remove("header__nav_active");
   });
+
+  if (window.innerWidth < 768) {
+    for (let i = 0; i < menuLinks.length; i++) {
+      menuLinks[i].addEventListener("click", () => {
+        menu.classList.remove("header__nav_active");
+      });
+    }
+  }
 })();
 
 // Scroll to anchors
@@ -32,14 +39,12 @@
     let targetPosition = target.getBoundingClientRect().top - headerElHeight;
     let startPosition = window.pageYOffset;
     let startTime = null;
-
     const ease = function (t, b, c, d) {
       t /= d / 2;
       if (t < 1) return (c / 2) * t * t + b;
       t--;
       return (-c / 2) * (t * (t - 2) - 1) + b;
     };
-
     const animation = function (currentTime) {
       if (startTime === null) startTime = currentTime;
       const timeElapsed = currentTime - startTime;
@@ -49,7 +54,6 @@
     };
     requestAnimationFrame(animation);
   };
-
   const scrollTo = function () {
     const links = document.querySelectorAll(".js-scroll");
     links.forEach((each) => {
